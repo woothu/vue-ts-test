8e74c5a58c1b9165be0229a9bfeeddafea285aa6
woothu <32914662+woothu@users.noreply.github.com>
2019-11-07 15:54:17 +0100

    refactor: update coreui-icons-vue, add vue module augmentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { CIconPlugin } from '@coreui/icons-vue';
+import { CIcon } from '@coreui/icons-vue';
 // import CoreuiVue from '@coreui/vue';
 import { pencil } from '@coreui/icons';
 
 Vue.config.productionTip = false;
 // Vue.use(CoreuiVue);
-Vue.use(CIconPlugin, { pencil });
+Vue.component('CIcon', CIcon);
 
 new Vue({
+  icons: { pencil },
   render: (h) => h(App),
 }).$mount('#app');
